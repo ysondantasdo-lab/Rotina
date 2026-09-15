56eedac9018aef0e885e1c85bf52d6d10ee8043c
Yson <ysondantasdo@gmail.com>
2026-09-15 15:30:27 +0000

    icone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "br.com.ysondantas.rotina"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildFeatures {
