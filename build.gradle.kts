188e780e4641f4e12b2e9a9aeae2bbc2ceed3abf
Yson <ysondantasdo@gmail.com>
2026-09-15 11:18:08 +0000

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ysondantas.rotina" // Identificador único do seu app
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.ysondantas.rotina"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Bibliotecas bases para a interface e funcionamento do Android
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+}
