7f333f138c1a5e14b89b8d41056282c51f0df3a7
Yson <ysondantasdo@gmail.com>
2026-09-16 08:16:45 -0300

    Update version code and name to 3 and 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "br.com.ysondantas.rotina"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildFeatures {
