61ffa8c3daac5f38018e25275707f60bd4a0e48b
Yson <ysondantasdo@gmail.com>
2026-09-23 10:42:20 -0300

    Update versionCode and versionName to 7 and 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "br.com.ysondantas.rotina"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
     }
 
     buildFeatures {
