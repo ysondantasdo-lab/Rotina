dc9fe249a379e484cb905b37e87219dfa1b365b6
Yson <ysondantasdo@gmail.com>
2026-09-23 10:41:34 -0300

    Add Firebase Auth dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
 }
